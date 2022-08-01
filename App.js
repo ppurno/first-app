d622ca7030c95b733398af546fa17b8338f235e8
ppurno <82290761+ppurno@users.noreply.github.com>
2022-08-01 15:20:11 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import MyComponent from './MyComponent';
+import './App.css';
+
+function App() {
+  return (
+    <MyComponent />  
+  );
+}
+
+export default App;
---
+++ MyComponent.jsx
@@ -0,0 +1,8 @@
+const MyComponent = () => {
+    return (    
+        <h1>Hello World</h1>  
+        );
+
+};
+
+export default MyComponent;
